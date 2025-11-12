43e106a296de8a2bcf2b9edd3dbd6afb8b96760e
CaduCTA15 <cadu031508@gmail.com>
2025-11-11 21:09:55 -0300

    yes

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ def index ():
 # Se for o modulo principal, roda o projeto em debug
 if __name__ == '__main__':
     app.run (debug = True)
+    app.run
+    @app.route('/api/tarefas/<int:todo_id', methods=['POST'])
+    def create_tarefa()
+        corpo = request.get_json()
+        tarefa_name = corpo.get('name')
+        tarefa_descrition = corpo.get('descrition')
+        criar_tarefa(tarefa_name, tarefa_descrition)
+        return{
+            'message' : 'tarefa cadastrada'
+        }
---
+++ tarefa.py
@@ -0,0 +1,47 @@
+from flask import jsonify
+from conexao import get_conexao
+from psycopg2.extras import RealDictCursor
+
+def buscar_tarefas():
+    con = get_conexao()
+    cursor = con.cursor(cursor_factory=RealDictCursor)
+    cursor.execute(
+        "SELECT id, name, description FROM todos"
+    )
+
+    todos = cursor.fetchall()
+
+    cursor.close()
+    con.close()
+
+    return jsonify(todos)
+
+def buscar_tarefa(id):
+    con = get_conexao()
+    cursor = con.cursor(cursor_factory=RealDictCursor)
+    cursor.execute(
+        "SELECT id, name, description FROM todos WHERE id = %s",
+        (id,)
+    )
+
+    todo = cursor.fetchone()
+
+    cursor.close()
+    con.close()
+
+    return jsonify(todo)
+
+def create(name, description):
+    con = get_conexao()
+    cursor = con.cursor()
+    cursor.execute(
+        "INSERT INTO todos (name, description) VALUES (%s, %s)",
+        (name, description)
+    )    
+
+    con.commit()
+
+    cursor.close()
+    con.close()
+
+    
